a9838b27d7e0c5ebd577b78bcb6db8e03768959e
Omar Juma <jumao2669@gmail.com>
2023-06-19 15:42:24 +0300

    split the likes into two variants. Working on comm

--- Components/UI/Input.js
+++ Components/UI/Input.js
@@ -1,8 +1,23 @@
+import React, {useState} from 'react';
 import {TouchableOpacity, View} from 'react-native';
 import {TextInput, Button, Avatar} from 'react-native-paper';
 import paperIcon from './paperIcon.png';
 import {primaryColor} from './AppBar';
+
 const Input = () => {
+  const [newComment, setNewComment] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const submitHandler = () => {
+    if (newComment.trim().length > 0) {
+      setIsValid(true);
+      console.log(newComment);
+      setNewComment('');
+    } else {
+      setIsValid(false);
+      console.log('empty message');
+    }
+  };
   return (
     <View
       style={{
@@ -12,14 +27,24 @@ const Input = () => {
         gap: 10,
       }}>
       <TextInput
-        label="Add a comment..."
+        label={isValid ? 'Add a comment...' : 'Please enter a comment'}
         multiline={true}
         numberOfLines={5}
-        underlineColor={primaryColor}
-        activeUnderlineColor={primaryColor}
-        style={{backgroundColor: '#f2f8f9', flex: 1, borderWidth:1, borderColor:primaryColor}}
+        underlineColor={isValid ? primaryColor : 'tomato'}
+        activeUnderlineColor={isValid ? primaryColor : 'tomato'}
+        value={newComment}
+        onChange={e => {
+          setIsValid(true);
+          setNewComment(e.nativeEvent.text);
+        }}
+        style={{
+          backgroundColor: '#f2f8f9',
+          flex: 1,
+          borderWidth: 1,
+          borderColor: isValid ? primaryColor : 'tomato',
+        }}
       />
-      <TouchableOpacity>
+      <TouchableOpacity onPress={submitHandler}>
         <Avatar.Icon
           style={{backgroundColor: primaryColor}}
           icon={paperIcon}
